bb0993f2245510a9c8c6c75127fcb43230b09825
Aiden Leach <leacha@my.lanecc.edu>
2023-12-03 19:35:51 -0800

    Initialized

---
+++ JJBAWiki/JJBAWiki/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JJBAWiki.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
